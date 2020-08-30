f647c7b323e580c2eb859986a7803e764dab2eb1
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-30 21:09:04 +0200

    implementation of Aircraft class according to UML and make sure id is unique... vehicles will inherit from Aircraft class

--- src/launcher/avaj/Aircraft.java
+++ src/launcher/avaj/Aircraft.java
@@ -1,4 +1,18 @@
 package launcher.avaj;
 
 public abstract class Aircraft {
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+    private long idCounter;
+
+    protected Aircraft(long id, String name, Coordinates coordinates) {
+        this.id = nextId();
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    private long nextId(){
+        return this.idCounter + 1;
+    }
 }
