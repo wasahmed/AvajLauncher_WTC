4fcfd7ae5431850de00d3c65441cf39ec1f51574
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-30 21:34:58 +0200

    implementation of the Coordinates class (composition).... The default constructor is a public no arguments constructor. To be more specific it has the same access level as the class, so public in a public class, private in a private class, etc.

--- src/launcher/avaj/Coordinates.java
+++ src/launcher/avaj/Coordinates.java
@@ -1,4 +1,25 @@
 package launcher.avaj;
 
 public class Coordinates {
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    public Coordinates(int longitude, int latitude, int height){
+        this.longitude =longitude;
+        this.latitude =latitude;
+        this.height = height;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
