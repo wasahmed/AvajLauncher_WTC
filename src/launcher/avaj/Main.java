920db28a9d02c16501adf0f06e8370bc0fd407a4
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-31 23:02:32 +0200

    should be it

--- src/launcher/avaj/Main.java
+++ src/launcher/avaj/Main.java
@@ -16,16 +16,24 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         try{
+            if (args.length > 1)
+                throw new Exception("only takes one argument");
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line = reader.readLine();
             if (line != null){
                 weatherTower = new WeatherTower();
                 int simulations = Integer.parseInt(line.split(" ")[0]);
+                String[] sims = line.split(" ");
+                if (sims.length != 1)
+                    throw new Exception("invalid 1st line");
                 if (simulations < 0){
                     System.out.println("Invalid simulations count " + simulations);
                     System.exit(1);
                 }
                 while ((line = reader.readLine()) != null){
+                    String[] count = line.split(" ");
+                    if (count.length != 5)
+                        throw new Exception("invalid scenario file");
                     Flyable flyable = AircraftFactory.newAircraft(line.split(" ")[0], line.split(" ")[1],
                             Integer.parseInt(line.split(" ")[2]), Integer.parseInt(line.split(" ")[3]),
                             Integer.parseInt(line.split(" ")[4]));
@@ -45,7 +53,9 @@ public class Main {
         }catch (IOException e){
             System.out.println("There was an error while reading the file " + args[0]);
         }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Specify simulation file");
+            System.out.println("Specify scenario file");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
