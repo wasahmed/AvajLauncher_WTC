ab6cac6ea89d7b1d11c4b9ad2ad28f9d887787af
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-31 14:55:05 +0200

    Vehicle classes implemented... Probably will need to write to the simulation.txt from each of the vehicles :construction:

--- src/launcher/avaj/vehicles/Baloon.java
+++ src/launcher/avaj/vehicles/Baloon.java
@@ -1,7 +1,41 @@
 package launcher.avaj.vehicles;
 
-import launcher.avaj.Aircraft;
-import launcher.avaj.Flyable;
+import launcher.avaj.*;
 
 public class Baloon extends Aircraft implements Flyable {
+    private WeatherTower weatherTower;
+
+    public Baloon(String name, Coordinates coordinates){
+        super(name, coordinates);
+    }
+
+    @Override
+    public void updateConditions() {
+        int updatedLongitude = this.coordinates.getLongitude();
+        int updatedLatitude = this.coordinates.getLatitude();
+        int updatedHeight = this.coordinates.getHeight();
+
+        switch (this.weatherTower.getWeather(this.coordinates)){
+            case "RAIN":
+                this.coordinates = new Coordinates(coordinates.getLongitude(), coordinates.getLatitude(), coordinates.getHeight() -5);
+                break;
+            case "FOG":
+                this.coordinates = new Coordinates(coordinates.getLongitude(), coordinates.getLatitude(), coordinates.getHeight() -3);
+                break;
+            case "SUN":
+                this.coordinates = new Coordinates(coordinates.getLongitude() + 2, coordinates.getLatitude(), coordinates.getHeight() + 4);
+                break;
+            case "SNOW":
+                this.coordinates = new Coordinates(coordinates.getLongitude(), coordinates.getLatitude(), coordinates.getHeight() -15);
+                break;
+        }
+        if (updatedHeight > 100)
+            this.coordinates = new Coordinates(updatedLongitude,updatedLatitude, 100);
+    }
+
+    @Override
+    public void registerTower(WeatherProvider weatherProvider) {
+        this.weatherTower = weatherTower;
+        this.weatherTower.register(this);
+    }
 }
