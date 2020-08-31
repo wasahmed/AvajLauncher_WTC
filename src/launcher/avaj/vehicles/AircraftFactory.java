ae6222c1cdf5b466b62b3e10cfc1c2a7e2a395e0
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-31 15:24:18 +0200

    Should be able to test now :construction_worker:

--- src/launcher/avaj/vehicles/AircraftFactory.java
+++ src/launcher/avaj/vehicles/AircraftFactory.java
@@ -1,4 +1,21 @@
 package launcher.avaj.vehicles;
 
+import launcher.avaj.Coordinates;
+import launcher.avaj.Flyable;
+
 public abstract class AircraftFactory {
+    public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+
+        Coordinates coordinates = new Coordinates(longitude, latitude, height);
+        switch (type.toUpperCase()) {
+            case "HELICOPTER":
+                return new Helicopter(name, coordinates);
+            case "JETPLANE":
+                return new JetPlane(name, coordinates);
+            case "BALOON":
+                return new Baloon(name, coordinates);
+            default:
+                return null;
+        }
+    }
 }
