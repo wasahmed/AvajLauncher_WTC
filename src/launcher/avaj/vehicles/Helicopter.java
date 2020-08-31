2a01cf063d29ba70f5975246726a50db297b528c
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-31 14:42:02 +0200

    Helicopter class implemented... Probably will need to write to the simulation.txt from each of the vehicles :construction:

--- src/launcher/avaj/vehicles/Helicopter.java
+++ src/launcher/avaj/vehicles/Helicopter.java
@@ -1,12 +1,9 @@
 package launcher.avaj.vehicles;
 
-import launcher.avaj.Aircraft;
-import launcher.avaj.Coordinates;
-import launcher.avaj.Flyable;
-import launcher.avaj.WeatherProvider;
+import launcher.avaj.*;
 
 public class Helicopter extends Aircraft implements Flyable {
-    private WeatherProvider weatherProvider;
+    private WeatherTower weatherTower;
 
     public Helicopter(String name, Coordinates coordinates){
         super(name, coordinates);
@@ -14,11 +11,31 @@ public class Helicopter extends Aircraft implements Flyable {
 
     @Override
     public void updateConditions() {
+        int updatedLongitude = this.coordinates.getLongitude();
+        int updatedLatitude = this.coordinates.getLatitude();
+        int updatedHeight = this.coordinates.getHeight();
 
+        switch (this.weatherTower.getWeather(this.coordinates)){
+            case "RAIN":
+                this.coordinates = new Coordinates(coordinates.getLongitude() + 5, coordinates.getLatitude(), coordinates.getHeight());
+                break;
+            case "FOG":
+                this.coordinates = new Coordinates(coordinates.getLongitude() + 1, coordinates.getLatitude(), coordinates.getHeight());
+                break;
+            case "SUN":
+                this.coordinates = new Coordinates(coordinates.getLongitude() + 10 , coordinates.getLatitude(), coordinates.getHeight() + 2);
+                break;
+            case "SNOW":
+                this.coordinates = new Coordinates(coordinates.getLongitude(), coordinates.getLatitude(), coordinates.getHeight() - 12);
+                break;
+        }
+        if (updatedHeight > 100)
+            this.coordinates = new Coordinates(updatedLongitude,updatedLatitude, 100);
     }
 
     @Override
-    public void registerTower() {
-
+    public void registerTower(WeatherProvider weatherProvider) {
+        this.weatherTower = weatherTower;
+        this.weatherTower.register(this);
     }
 }
